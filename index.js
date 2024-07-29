e400a780d9b94eff137b6c74a1166646c2cfb76b
chico <99514791+Perlesvaux@users.noreply.github.com>
2024-07-29 15:48:08 -0600

    feat: still working on sqlite3 integration. Planning on moving to android development

--- index.js
+++ index.js
@@ -9,27 +9,40 @@ app.use(bodyParser.urlencoded({extended:false}))
 
 // sql --username=chico --dbname=jaap --tuples-only --no-align
 // Configure connection to psql database
-import Pool from 'pg-pool';
-const connection = new Pool({
-  user:"chico",
-  host:"localhost",
-  database:"jaap",
-  password:"elpepe",
-  port: 5432
-});
+// import Pool from 'pg-pool';
+// const connection = new Pool({
+//   user:"chico",
+//   host:"localhost",
+//   database:"jaap",
+//   password:"elpepe",
+//   port: 5432
+// });
 
 // Wrapper that handles PSQL queries
-async function q (query, params){
-  try {
-    const result = await connection.query(query, params);
-    return result.rows;
-    
-  } catch (err) {
-    console.error(err);
-    return "";
-  }
+// async function q (query, params){
+//   try {
+//     const result = await connection.query(query, params);
+//     return result.rows;
+//     
+//   } catch (err) {
+//     console.error(err);
+//     return "";
+//   }
+// }
 
-}
+
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+// const db = new sqlite3.Database('./jaap.db', (err)=>{
+//   if ( err ) { console.error("Can't open db =(") }
+//   else {console.log("Connected to database!")}
+// })
+
+const dbPromise = open({
+  filename:'./jaap.db',
+  driver: sqlite3.Database
+})
 
 // Logger
 function logger (req, res, next){
@@ -46,49 +59,50 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', logger, async function (req, res) {
-  const usuarios = await q("select * from usuarios")
+  const db = await dbPromise;
+  const usuarios = await db.all("select * from usuarios");
+  // const usuarios = await q("select * from usuarios")
+  await db.close
   res.json({msg: "Hello World!", dbquery: usuarios})
 });
 
 app.get('/encontrar', logger, async function(req, res) {
+  const db = await dbPromise;
   let response = {dbquery:"Sorry, that user doesn't exist"}
-  let strQuery = "SELECT * FROM usuarios FULL JOIN recibos USING(medidor) WHERE medidor=$1 ORDER BY desde DESC LIMIT 1"
-
-  
-  // const usuario = await q('select * from usuarios where medidor=$1', [req.body.medidor])
-  const usuario = await q(strQuery, [req.body.medidor])
-  // if (usuario) response={...response, dbquery:`Welcome back ${usuario[0].nombre}, your last consumption was ${usuario[0].lectura_actual}`}
+  let strQuery = "SELECT * FROM usuarios JOIN recibos USING(medidor) WHERE medidor=(?) ORDER BY desde DESC LIMIT 1"
+  const usuario = await db.all(strQuery, [req.body.medidor])
   if (usuario) response={...response, dbquery:usuario, msg:`La ultima lectura de ${usuario[0].nombre} fue ${usuario[0].lectura_actual} (${usuario[0].hasta})` }
-  
-  // res.json({dbquery: `Welcome back ${usuario[0].nombre}!`})
+  await db.close();
   res.json(response)
 })
 
-app.get('/todos-los-recibos-de', logger, async function(req, res){
-  let response = {dbquery:"Sorry, no receipts were found"}
-  let strQuery = "SELECT * FROM usuarios FULL JOIN recibos USING(medidor) WHERE medidor=$1"
-  const recibos = await q(strQuery, [req.body.medidor])
-  if (recibos) response={...response, dbquery:recibos}
-  res.json(response)
 
-   // insert into recibos values(503,270,261,9,'2023-11-15','2023-12-15',31,99891)
+// app.get('/todos-los-recibos-de', logger, async function(req, res){
+// })
 
-})
+// app.get('/todos-los-recibos-de', logger, async function(req, res){
+//   let response = {dbquery:"Sorry, no receipts were found"}
+//   let strQuery = "SELECT * FROM usuarios FULL JOIN recibos USING(medidor) WHERE medidor=$1"
+//   const recibos = await q(strQuery, [req.body.medidor])
+//   if (recibos) response={...response, dbquery:recibos}
+//   res.json(response)
+//
+//    // insert into recibos values(503,270,261,9,'2023-11-15','2023-12-15',31,99891)
+//
+// })
 
-app.get('/generar-recibo', logger, async function(req, res){
-  let response = {dbquery: "Sorry, something went wrong"}
-  let strQuery =  "SELECT * FROM usuarios FULL JOIN recibos USING(medidor) WHERE medidor=$1 ORDER BY desde DESC LIMIT 1"
-  const usuario = await q(strQuery, [req.body.medidor])
-  const consumption = req.body.lectura_nueva - usuario[0].lectura_actual 
-  const until = new Date(usuario[0].hasta)
-  until.setDate(until.getDate()+30);
-  let new_data = {...usuario[0], hasta:until, desde:usuario[0].hasta}
-  console.log(until)
-  console.log(consumption)
-  res.json(new_data)
-
-
-})
+// app.get('/generar-recibo', logger, async function(req, res){
+//   let response = {dbquery: "Sorry, something went wrong"}
+//   let strQuery =  "SELECT * FROM usuarios FULL JOIN recibos USING(medidor) WHERE medidor=$1 ORDER BY desde DESC LIMIT 1"
+//   const usuario = await q(strQuery, [req.body.medidor])
+//   const consumption = req.body.lectura_nueva - usuario[0].lectura_actual 
+//   const until = new Date(usuario[0].hasta)
+//   until.setDate(until.getDate()+30);
+//   let new_data = {...usuario[0], hasta:until, desde:usuario[0].hasta}
+//   console.log(until)
+//   console.log(consumption)
+//   res.json(new_data)
+// })
 
 
 
