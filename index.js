4dd6de60c26f90fc4ce0acc504b49488e3af7f81
Perlesvaux <paltryhumanity@gmail.com>
2024-08-08 02:34:26 -0600

    refactor/singleton in RESPONSE and QUERY. Cleaned up comments

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 const app = express();
 
@@ -27,8 +26,8 @@ async function q (query, params){
   } catch (err) {
     console.error(err);
     return "";
-  }
 
+  }
 }
 
 // Logger
@@ -40,7 +39,6 @@ function logger (req, res, next){
 // Configure CORS
 import cors from 'cors';
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
 // http://expressjs.com/en/starter/static-files.html
 // app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
@@ -52,37 +50,35 @@ let QUERY = ''
 app.get('/', logger, async function (req, res) {
   try {
     RESPONSE = [{msg: "Bienvenido al JAAP"}];
+
   } catch (err) {
     console.error(`Hubo un error al intentar acceder a la tabla jaap.usuarios: ${err}`);
+
   }
     res.send(RESPONSE);
 });
+
 
 app.get('/usuarios', logger, async function (req, res) {
   try {
     RESPONSE = await q("select * from usuarios");
-    //res.send(RESPONSE);
+
   } catch (err) {
     console.error(`Hubo un error al intentar acceder a la tabla jaap.usuarios: ${err}`);
-    //res.send([]);
+
   }
     res.send(RESPONSE);
 });
 
+
 app.get('/ultimo', logger, async function(req, res) {
   try {
-    //let response = {dbquery:"Sorry, that user doesn't exist"}
     QUERY = "SELECT * FROM usuarios FULL JOIN recibos USING(medidor) WHERE medidor=$1 ORDER BY desde DESC LIMIT 1"
-    // const usuario = await q('select * from usuarios where medidor=$1', [req.body.medidor])
     RESPONSE = await q(QUERY, [req.body.medidor])
-    // if (usuario) response={...response, dbquery:`Welcome back ${usuario[0].nombre}, your last consumption was ${usuario[0].lectura_actual}`}
-    //if (usuario) response={...response, dbquery:usuario, msg:`La ultima lectura de ${usuario[0].nombre} fue ${usuario[0].lectura_actual} (${usuario[0].hasta})` }
 
-    // res.json({dbquery: `Welcome back ${usuario[0].nombre}!`})
-    //res.send(RESPONSE)
   } catch (err) {
     console.error(`Hubo un error al intentar acceder a la tabla jaap.usuarios FULL JOIN jaap.recibos: ${err}`);
-    //res.send(RESPONSE)
+
   }
     res.send(RESPONSE)
 })
@@ -92,45 +88,35 @@ app.get('/recientes', logger, async function (req, res) {
   try {
     QUERY = "select DISTINCT ON (medidor) * from usuarios full join recibos USING(medidor) order by medidor, desde DESC"
     RESPONSE = await q(QUERY);
-    //res.send(RESPONSE)
-    
 
   } catch(err) {
     console.error(`Hubo un error al intentar acceder a la tabla jaap.usuarios FULL JOIN jaap.recibos: ${err}`);
-    //res.send(RESPONSE)
+
   }
     res.send(RESPONSE)
-
 })
 
 app.get('/todos-los-recibos-de', logger, async function(req, res){
   try {
-    //let response = {dbquery:"Sorry, no receipts were found"}
     QUERY = "SELECT * FROM usuarios FULL JOIN recibos USING(medidor) WHERE medidor=$1"
     RESPONSE = await q(QUERY, [req.body.medidor])
-    //if (recibos) response={...response, dbquery:recibos}
-    //res.send(RESPONSE)
-    // insert into recibos values(503,270,261,9,'2023-11-15','2023-12-15',31,99891)
+
   } catch (err) {
     console.error(`Hubo un error al intentar acceder a la tabla jaap.usuarios FULL JOIN jaap.recibos: ${err}`);
-    //res.send(RESPONSE)
+
   }
     res.send(RESPONSE);
-
 })
 
+
 app.get('/generar-recibo', logger, async function(req, res){
-  //let response = {dbquery: "Sorry, something went wrong"}
   QUERY =  "SELECT * FROM usuarios FULL JOIN recibos USING(medidor) WHERE medidor=$1 ORDER BY desde DESC LIMIT 1"
   const usuario = await q(QUERY, [req.body.medidor])
-
   const consumption = parseInt(req.body.lectura_nueva) - parseInt(usuario[0].lectura_actual) 
-  //const consumption = parseInt(req.body.lectura_nueva) - parseInt(usuario[0].lectura_actual) 
   const until = new Date(usuario[0].hasta)
   const today = new Date(Date.now())
-
   const numberOfDays = Math.ceil((today-until) / (1000 * 60 * 60 * 24))
-  //until.setDate(until.getDate()+30);
+
   let new_data = {...usuario[0],
     desde:usuario[0].hasta,
     hasta:today,
